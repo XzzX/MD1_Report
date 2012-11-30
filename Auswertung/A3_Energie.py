5fdb354176c80acae8c2008593c105130388ec1f
XzzX <xzzx@xzzx.info>
2012-11-30 14:44:59 +0100

    Diffusionsplots

--- Auswertung/A3_Energie.py
+++ Auswertung/A3_Energie.py
@@ -11,9 +11,13 @@ _vy = 5
 _vz = 6
 _ekin = 7
 _epot = 8
-A101 = np.loadtxt("Impuls/L1V05dt01.txt").transpose()
-A1001 = np.loadtxt("Impuls/L1V05dt0001.txt").transpose()
-A1005 = np.loadtxt("Impuls/L1V05dt0005.txt").transpose()
+#A101 = np.loadtxt("Impuls/L1V05dt01.txt").transpose()
+#A1001 = np.loadtxt("Impuls/L1V05dt001.txt").transpose()
+#A1005 = np.loadtxt("Impuls/L1V05dt005.txt").transpose()
+
+A101 = np.loadtxt("Impuls/L14V05.txt").transpose()
+A1001 = np.loadtxt("Impuls/Eulerdt001.txt").transpose()
+A1005 = np.loadtxt("Impuls/Eulerdt005.txt").transpose()
 
 #pl.figure(figsize=(12, 9))
 
@@ -26,11 +30,13 @@ pl.plot(A1001[_t], E1001, 'x', label = ur"$\Delta t = 0.01$")
 pl.plot(A1005[_t], E1005, 'x', label = ur"$\Delta t = 0.05$")
 pl.plot(A101[_t], E101, 'x', label = ur"$\Delta t = 0.1$")
 pl.yscale("log")
+pl.xscale("log")
 pl.xlabel("Zeit t [a.u.]")
 pl.ylabel("Energie E [a.u.]")
 pl.legend(loc = "upper left")
 pl.grid()
 
+pl.savefig("A3EnergieEuler.png", dpi = 300)
 pl.show()
 
 #FitFunc = lambda a,b,x : a*x + b
